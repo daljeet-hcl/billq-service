cf8dbb748984302b72a39b24e25a624a3e82a29c
Karthik Gunasekaran <gunakarthik@gmail.com>
2020-05-16 15:59:57 +0530

    initial commit

---
+++ src/main/java/com/demo/billq/BillqServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.billq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillqServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillqServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/billq/BillqServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.billq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillqServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
